94c7ad69aca12f64a6ff81b4cf2e2440948b81b7
edu <edu.fablabbcn.2022@gmail.com>
2023-08-03 17:38:48 +0300

    Theekshana approves the code

--- good/if_sensor_then_spin/if_sensor_then_spin.ino
+++ good/if_sensor_then_spin/if_sensor_then_spin.ino
@@ -1,20 +1,20 @@
-#include <Stepper.h>
-const int stepsPerRevolution_default = 1500;
-const int rpm = 15;
-Stepper stepper1 = Stepper(stepsPerRevolution_default, 4, 5, 6, 7);
+#include <AccelStepper.h>;
+
+#define STEPPER_SPEED 100
+#define STEPPER_MAX_SPEED 1000
+
+AccelStepper stepper(1, 2, 3); // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+//(::driver, step, dir)
+
 
 const int trigPin = 6;  // TRIG pin
 const int echoPin = 5;  // ECHO pin
-const int ledPin = 13;  // transmit LED
 const int redLed = 13;
 const int greenLed = 12;
 const int threshold_distance = 20;
 
-//const int nemaStepPin = 1;
-//const int nemaDirectionPin = 0;
-//const int stepsPerRevolution_nema = 200;
-
 float duration_us, distance_cm;
+bool hand_detected = false;
 
 
 void setup() {
@@ -25,27 +25,28 @@ void setup() {
 
 void enable_io_pins() {
   pinMode(trigPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
   pinMode(greenLed, OUTPUT);
   pinMode(redLed, OUTPUT);
   pinMode(echoPin, INPUT);
-
-  pinMode(nemaStepPin, OUTPUT);
-  pinMode(nemaDirectionPin, OUTPUT);
-
 }
 
 void enable_stepper_motors() {
-  stepper1.setSpeed(rpm); // crappy motor used on monday
-  digitalWrite(nemaDirectionPin, HIGH);  // good nema motor
+   stepper.setMaxSpeed(STEPPER_MAX_SPEED);
+   stepper.setSpeed(STEPPER_SPEED); 
 }
 
 int hands_distance() {
-  digitalWrite(trigPin, HIGH);
-  digitalWrite(ledPin, HIGH);
-  delay(100);
-  digitalWrite(trigPin, LOW);
-  digitalWrite(ledPin, LOW);
+  if(!hand_detected){
+    digitalWrite(trigPin, HIGH);
+    delay(100);
+    digitalWrite(trigPin, LOW);
+  }else{
+    digitalWrite(trigPin, HIGH);
+    digitalWrite(redLed, HIGH);
+    delay(100);
+    digitalWrite(trigPin, LOW);
+    digitalWrite(redLed, LOW);
+  }
 
   // measure duration of pulse from ECHO pin
   duration_us = pulseIn(echoPin, HIGH);
@@ -60,31 +61,18 @@ void loop(){
   // print the value to Serial Monitor
   Serial.println(distance_cm);
 
-  if (distance_cm < threshold_distance) {
+  if (distance_cm > 0 && distance_cm < threshold_distance) {
     green_led();
     dispense_sanitiser();
   } else {
-    delay(100);
     red_led();
   }
   delay(100);
 }
 
 void dispense_sanitiser(){
-  stepper1.step(stepsPerRevolution_default);
-  delay(1000);
-  stepNema(1000);
+  stepper.runSpeed();
 }
-
-// void stepNema(int numSteps){
-//   const int pause_in_microseconds = 1000;
-//   for(int x = 0; x < numSteps; x++){
-//     digitalWrite(nemaStepPin, HIGH);
-//     delayMicroseconds(pause_in_microseconds);
-//     digitalWrite(nemaStepPin, LOW);
-//     delayMicroseconds(pause_in_microseconds);
-//   } 
-// }
 
 void red_led(){
     digitalWrite(greenLed, LOW);
@@ -94,4 +82,5 @@ void red_led(){
 void green_led(){
     digitalWrite(redLed, LOW);
     digitalWrite(greenLed, HIGH);
+    delay(5000);
 }
